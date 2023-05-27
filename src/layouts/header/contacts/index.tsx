de699b851f16eb7b14627ca86f692fc765e015ef
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-05-27 15:45:16 +0300

    fixed paid

--- src/layouts/header/contacts/index.tsx
+++ src/layouts/header/contacts/index.tsx
@@ -14,7 +14,7 @@ const Contacts = () => {
         <>
             <div className={s.contacts}>
                 <div className={s.contacts__address}>
-                    <Link href="#" className={s.contacts__address_link}>
+                    <Link href="https://www.google.com/maps/place/%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%9E%D0%BB%D1%8C%D0%B3%D0%B8+%D0%91%D0%B0%D1%81%D0%B0%D1%80%D0%B0%D0%B1,+15,+%D0%A1%D1%82%D1%80%D0%B8%D0%B9,+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+82401/@49.2665923,23.87411,17z/data=!3m1!4b1!4m6!3m5!1s0x473a699251572083:0xba705f0698fdb950!8m2!3d49.2665923!4d23.87411!16s%2Fg%2F11c43_25x9?entry=ttu" className={s.contacts__address_link}>
                         <Image src={pointer} alt="Pointer" width={22} height={28} />
                     </Link>
                     <Image className={s.contacts__address_img} src={pointer} alt="Pointer" width={22} height={28} />
--- src/pages/paid/index.tsx
+++ src/pages/paid/index.tsx
@@ -5,77 +5,82 @@ import s from './Paid.module.scss';
 import Container from '@/components/container';
 
 const data = [
-    {text: 'Масаж', price: 100},
-    {text: 'Фізіотерапевтичні процедури', price: 300}
+    { title: "Оздоровчі масажі та фізіотерапевтичні процедури", text1: 'Масаж', price1: 100, text2: 'Фізіотерапевтичні процедури', price2: 300 },
+    { title: "Медичні огляди та видача бланків особистої медичної книжки", text1: 'Масаж', price1: 100, text2: 'Фізіотерапевтичні процедури', price2: 300 },
+    { title: "Рентгенологія та комп’ютерна томографія ", text1: 'Масаж', price1: 100, text2: 'Фізіотерапевтичні процедури', price2: 300 },
+    { title: "Функціональна діагностика та УЗД", text1: 'Масаж', price1: 100, text2: 'Фізіотерапевтичні процедури', price2: 300 },
 ];
 
 const Paid = () => {
     return (
         <>
             <div className={s.paid}>
-                <Container>
-                    <div className={s.paid__info}>
-                        <h1 className={s.paid__info_title}>
+                <div className={s.paid__wrapper}>
+                    <Container>
+                        <h1 className={s.paid__title}>
                             Платні послуги
                         </h1>
-
-                        <p className={s.paid__info_subTitle}>
-                            В нашій лікарні ви можете отримати платні послуги,<br />
+                        <p className={s.paid__subTitle}>
+                            В нашій лікарні ви можете отримати платні послуги,
                             які ви можете перегоянути нижче
                         </p>
+                        {data.map((data) => (
+                            <div className={s.paid__info}>
 
-                        <div className={s.paid__info_table}>
-                            <div className={s.paid__info_table_title}>
-                                <p style={{
-                                    margin: '0',
-                                    padding: '20px'
-                                }}>
-                                    Оздоровчі масажі та фізіотерапевтичні процедури
-                                </p>
-                            </div>
+                                <div className={s.paid__info_table}>
+                                    <div className={s.paid__info_table_title}>
+                                        <p style={{
+                                            margin: '0',
+                                            padding: '20px'
+                                        }}>
+                                            {data.title}
+                                        </p>
+                                    </div>
 
-                            <div className={s.paid__info_table_info}>
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <td style={{
-                                                color: '#007E60',
-                                                borderLeft: '0px'
-                                            }}>
-                                                Назва послуги
-                                            </td>
-                                            <td style={{
-                                                color: '#007E60'
-                                            }}>
-                                                Ціна, грн
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style={{
-                                                borderLeft: '0px'
-                                            }}>
-                                                Масаж
-                                            </td>
-                                            <td>
-                                                100
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style={{
-                                                borderLeft: '0px'
-                                            }}>
-                                                Фізіотерапевтичні процедури
-                                            </td>
-                                            <td>
-                                                300
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                    <div className={s.paid__info_table_info}>
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td style={{
+                                                        color: '#007E60',
+                                                        borderLeft: '0px'
+                                                    }}>
+                                                        Назва послуги
+                                                    </td>
+                                                    <td style={{
+                                                        color: '#007E60'
+                                                    }}>
+                                                        Ціна, грн
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td style={{
+                                                        borderLeft: '0px'
+                                                    }}>
+                                                        {data.text1}
+                                                    </td>
+                                                    <td>
+                                                        {data.price1}
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td style={{
+                                                        borderLeft: '0px'
+                                                    }}>
+                                                        {data.text2}
+                                                    </td>
+                                                    <td>
+                                                        {data.price2}
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </Container>
+                        ))}
+                    </Container>
+                </div>
             </div>
         </>
     )
