e34b5ca5fdd799f53592eabb54108fbdd04bb8e4
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-05-27 14:32:56 +0300

    fixed ratePage

--- src/pages/rateWork/index.tsx
+++ src/pages/rateWork/index.tsx
@@ -10,87 +10,44 @@ import Container from '@/components/container';
 //img
 import rightArrow from '../../assets/img/rateWorkPage/rightArrow.png';
 
+const links = [
+    { text: "Оцініть Ваш візит в нашу лікарню", path: "/ocinit-vash-visit", },
+    { text: "Як Ви оцінюєте чистоту в нашому закладі?", path: "/ocinit-vash-visit", },
+    { text: "Чи був ввічливим з Вами персонал?", path: "/ocinit-vash-visit", },
+];
+
 const rateWork = () => {
     return (
         <>
             <div className={s.rateWork}>
-                <Container>
-                    <div className={s.rateWork__navBar}>
-                        <div className={s.rateWork__navBar_title}>
-                            <h1 className={s.rateWork__navBar_title_text}>
-                                Нам важлива Ваша думка
-                            </h1>
+                <div className={s.rateWork__wrapper}>
+                    <Container>
+                        <div className={s.rateWork__navBar}>
+                            <div className={s.rateWork__navBar_title}>
+                                <h1 className={s.rateWork__navBar_title_text}>
+                                    Нам важлива Ваша думка
+                                </h1>
+                            </div>
+                            {links.map((link) => (
+                                <Link className={s.rateWork__navBar_link} href={link.path}>
+                                    <div className={s.rateWork__navBar_link_info}>
+                                        <p className={s.rateWork__navBar_link_info_text}>
+                                            {link.text}
+                                        </p>
+                                        <div className={s.rateWork__navBar_link_info_img}>
+                                            <Image
+                                                src={rightArrow}
+                                                width={42}
+                                                height={42}
+                                                alt="Продовжити"
+                                            />
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))}
                         </div>
-
-                        <Link className={s.rateWork__navBar_link} href='#'>
-                            <div className={s.rateWork__navBar_link_info}>
-                                <p className={s.rateWork__navBar_link_info_text}>
-                                    Оцініть Ваш візит в нашу лікарню
-                                </p>
-
-                                <div className={s.rateWork__navBar_link_info_img}>
-                                    <Image
-                                        src={rightArrow}
-                                        width={42}
-                                        height={42}
-                                        alt="Продовжити"
-                                    />
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link className={s.rateWork__navBar_link} href='#'>
-                            <div className={s.rateWork__navBar_link_info}>
-                                <p className={s.rateWork__navBar_link_info_text}>
-                                    Як Ви оцінюєте чистоту в нашому закладі?
-                                </p>
-
-                                <div className={s.rateWork__navBar_link_info_img}>
-                                    <Image
-                                        src={rightArrow}
-                                        width={42}
-                                        height={42}
-                                        alt="Продовжити"
-                                    />
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link className={s.rateWork__navBar_link} href='#'>
-                            <div className={s.rateWork__navBar_link_info}>
-                                <p className={s.rateWork__navBar_link_info_text}>
-                                    Оцініть Ваш візит в нашу лікарню
-                                </p>
-
-                                <div className={s.rateWork__navBar_link_info_img}>
-                                    <Image
-                                        src={rightArrow}
-                                        width={42}
-                                        height={42}
-                                        alt="Продовжити"
-                                    />
-                                </div>
-                            </div>
-                        </Link>
-
-                        <Link className={s.rateWork__navBar_link} href='#'>
-                            <div className={s.rateWork__navBar_link_info}>
-                                <p className={s.rateWork__navBar_link_info_text}>
-                                    Чи був ввічливим з Вами персонал?
-                                </p>
-
-                                <div className={s.rateWork__navBar_link_info_img}>
-                                    <Image
-                                        src={rightArrow}
-                                        width={42}
-                                        height={42}
-                                        alt="Продовжити"
-                                    />
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                </Container>
+                    </Container>
+                </div>
             </div>
         </>
     )
