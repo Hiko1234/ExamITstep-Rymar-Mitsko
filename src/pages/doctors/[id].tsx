6bb10ff77882a5d250039077d28a8afe9c2d204c
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-05-27 15:07:15 +0300

    singleDoctorPage final

--- src/pages/doctors/[id].tsx
+++ src/pages/doctors/[id].tsx
@@ -9,6 +9,8 @@ import dataDoctors from "@/components/data/doctors"
 import CardDoctors from '@/components/card/cardDoctors'
 // import router
 import { useRouter } from "next/router";
+// import link
+import Link from 'next/link';
 
 const SingleDoctors = () => {
   // Отримання id з router.query
@@ -16,7 +18,9 @@ const SingleDoctors = () => {
   const { id } = router.query;
 
   const [data, setData] = useState(dataDoctors);
+  const [dataSpecialty, setDataSpecialty] = useState(dataDoctors)
   const [doctor, setDoctor] = useState(getObjectById(id));
+  const [doctorSpecialty, setDoctorSpecialty] = useState(getDoctorBySpecialty(doctor));
 
   // Функція для отримання об'єкта за ідентифікатором
   function getObjectById(id: any) {
@@ -27,10 +31,19 @@ const SingleDoctors = () => {
     })
     return targetDoctor[0]
   }
+  // Функція для отримання лікарів певної спеціальності
+  function getDoctorBySpecialty(doctor: any) {
+    const targetDoctors = dataSpecialty.filter((el) => {
+      if (el.specialty == doctor.specialty) {
+        return el
+      }
+    })
+    return targetDoctors
+  }
 
   useEffect(() => {
-    getObjectById(id)
-  }, [])
+    getObjectById(id);
+  }, []);
 
   return (
     <>
@@ -42,17 +55,25 @@ const SingleDoctors = () => {
               <div className={s.singleDoctor__card_data}>
                 <h4 className={s.singleDoctor__card_dataName}>{doctor?.lastName} {doctor?.name} {doctor?.fatherName}</h4>
                 <p className={s.singleDoctor__card_dataSpecialty}>{doctor?.specialty}</p>
-                <div className={s.singleDoctor__dataDepartament}>
+                <Link className={s.singleDoctor__dataDepartament} href="/departament">
                   <div className={s.singleDoctor__dataDepartament_imgWrapper}>
                     <Image className={s.singleDoctor__dataDepartament_img} src={doctor?.departamentImg} alt={doctor?.department} width={32} height={32} />
                   </div>
                   <p className={s.singleDoctor__dataDepartament_name}>{doctor?.department}</p>
-                </div>
+                </Link>
                 <div className={s.singleDoctor__card_info}><span className={s.singleDoctor__card_infoTitle}>Кабінет: </span><span className={s.singleDoctor__card_infoData}>{doctor?.office}</span></div>
                 <div className={s.singleDoctor__card_info}><span className={s.singleDoctor__card_infoTitle}>Категорія: </span><span className={s.singleDoctor__card_infoData}>{doctor?.category}</span></div>
                 <div className={s.singleDoctor__card_info}><span className={s.singleDoctor__card_infoTitle}>Освіта: </span><span className={s.singleDoctor__card_infoData}>{doctor?.education}</span></div>
                 <div className={s.singleDoctor__card_info}><span className={s.singleDoctor__card_infoTitle}>Стаж: </span><span className={s.singleDoctor__card_infoData}>{doctor?.experience}</span></div>
               </div>
+            </div>
+            <h4 className={s.singleDoctor__specialtyTitle}>Лікарі цієї ж спеціалізації</h4>
+            <div className={s.doctors__cards}>
+              {doctorSpecialty.map((data) => (
+                <>
+                  <CardDoctors data={data} />
+                </>
+              ))}
             </div>
           </Container>
         </div>
