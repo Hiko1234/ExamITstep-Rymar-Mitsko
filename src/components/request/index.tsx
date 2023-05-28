1b64e17daa1cf61f90f14259b31674b348b7cd6e
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-05-28 14:49:45 +0300

    checkbox

--- src/components/request/index.tsx
+++ src/components/request/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import s from "./Request.module.scss"
 // import img 
 import Image from 'next/image';
-import icon from "../../assets/img/homePage/nav/declaration.png"
+import icon from "../../assets/img/departmentPage/department1.png"
 
 const RequestItem = () => {
     const [checkbox, setCheckbox] = useState<boolean>(false);
@@ -15,21 +15,23 @@ const RequestItem = () => {
                 <textarea className={s.requestForm__textarea} placeholder='Ваше повідомлення...'></textarea>
             </form>
             <div className={s.requestForm__btn}>
-                <div className={s.requestForm__btn_checkbox}><input type="checkbox" onClick={() => {
-                    setCheckbox(true);
+                <div className={s.requestForm__btn_checkbox}><input type="checkbox" onFocus={() => {
+                    setCheckbox(true)
                 }} /><label>Я хочу отримати офіційну відповідь на email</label></div>
                 <button className={s.requestForm__btn_button} onClick={() => {
                     setPopup(true);
                 }}>НАДІСЛАТИ</button>
             </div>
             {popup && (
-                <div className={s.popup}>
-                    <div className={s.popup__box} onClick={() => {
-                        setPopup(false)
-                    }}>
+                <div className={s.popup} onClick={() => {
+                    setPopup(false)
+                }}>
+                    <div className={s.popup__box}>
                         <div className={s.popup__box_container}>
-                            <Image className={s.popup__box_img} src={icon} alt="Popup" width={35} height={35} />
+                            <Image className={s.popup__box_img} src={icon} alt="Popup" width={100} height={100} />
                             <p className={s.popup__box_text}>Ваше повідомлення відправлене</p>
+                            {checkbox ? (<p className={s.popup__box_text}>Вам прийде відповідь на email</p>) : null}
+                            
                             <button className={s.popup__box_btn} onClick={() => {
                                 setPopup(false)
                             }}>Закрити</button>
