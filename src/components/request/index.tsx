509420a0023a395cff8a86cb9cd396e708d9564e
Hiko1234 <andriym1404@gmail.com>
2023-05-28 20:31:10 +0300

    request final

--- src/components/request/index.tsx
+++ src/components/request/index.tsx
@@ -8,39 +8,64 @@ import icon from "../../assets/img/departmentPage/department1.png"
 const RequestItem = () => {
     const [checkbox, setCheckbox] = useState<boolean>(false);
     const [popup, setPopup] = useState<boolean>(false);
+    const [textarea, setTextarea] = useState("");
 
     return (
         <>
             <form className={s.requestForm}>
-                <textarea className={s.requestForm__textarea} placeholder='Ваше повідомлення...'></textarea>
+                <textarea className={s.requestForm__textarea} placeholder='Ваше повідомлення...' onChange={(e) => {
+                    setTextarea(e.target.value);
+                }}></textarea>
             </form>
             <div className={s.requestForm__btn}>
-                <div className={s.requestForm__btn_checkbox}><input type="checkbox" onFocus={() => {
-                    setCheckbox(true)
-                }} /><label>Я хочу отримати офіційну відповідь на email</label></div>
+                <div className={s.requestForm__btn_checkbox}>
+                    <input
+                        type="checkbox"
+                        onChange={() => setCheckbox(!checkbox)}
+                    />
+                    <label>Я хочу отримати офіційну відповідь на email</label>
+                </div>
                 <button className={s.requestForm__btn_button} onClick={() => {
-                    setPopup(true);
+                    setPopup(true)
                 }}>НАДІСЛАТИ</button>
             </div>
-            {popup && (
-                <div className={s.popup} onClick={() => {
-                    setPopup(false)
-                }}>
-                    <div className={s.popup__box}>
-                        <div className={s.popup__box_container}>
-                            <Image className={s.popup__box_img} src={icon} alt="Popup" width={100} height={100} />
-                            <p className={s.popup__box_text}>Ваше повідомлення відправлене</p>
-                            {checkbox ? (<p className={s.popup__box_text}>Вам прийде відповідь на email</p>) : null}
-                            
-                            <button className={s.popup__box_btn} onClick={() => {
-                                setPopup(false)
-                            }}>Закрити</button>
+            {textarea.length > 0 ? (
+                <>
+                    {popup && (
+                        <div className={s.popup}>
+                            <div className={s.popup__box}>
+                                <div className={s.popup__box_container}>
+                                    <Image className={s.popup__box_img} src={icon} alt="Popup" width={100} height={100} />
+                                    <p className={s.popup__box_text}>Ваше повідомлення відправлене</p>
+                                    {checkbox ? (
+                                        <p className={s.popup__box_text}>Вам прийде відповідь на email</p>
+                                    ) : null}
+                                    <button className={s.popup__box_btn} onClick={() => setPopup(false)}>Закрити</button>
+                                </div>
+                            </div>
+                            <div className={s.popup__bg} onClick={() => setPopup(false)}></div>
                         </div>
-                    </div>
-                </div>
+                    )}
+                </>
+            ) : (
+                <>
+                    {popup && (
+                        <div className={s.popup}>
+                            <div className={s.popup__box}>
+                                <div className={s.popup__box_container}>
+                                    <Image className={s.popup__box_img} src={icon} alt="Popup" width={100} height={100} />
+                                    <p className={s.popup__box_text}>Будь ласка, введіть дані в поле вводу</p>
+                                    <button className={s.popup__box_btn} onClick={() => setPopup(false)}>Закрити</button>
+                                </div>
+                            </div>
+                            <div className={s.popup__bg} onClick={() => setPopup(false)}></div>
+                        </div>
+                    )}
+                </>
             )}
         </>
-    )
-}
+    );
+};
+
 
 export default RequestItem
