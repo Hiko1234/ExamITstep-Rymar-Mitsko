7b158773ff481617f01ae29463395b70cf41c678
Demian Rymar <ua.demyan@gmail.com>
2023-05-28 09:38:55 +0300

    HomePage fixed; Added charity

--- src/components/homePage/navigation/index.tsx
+++ src/components/homePage/navigation/index.tsx
@@ -1,39 +1,42 @@
-import React from 'react'
+import React from 'react';
 // import styles
-import s from "./HomeNav.module.scss"
+import s from "./HomeNav.module.scss";
 // import link
-import Link from 'next/link'
-// import img
+import Link from 'next/link';
+// import Image
 import Image from 'next/image'
-import icon1 from "../../../assets/img/homePage/nav/calendar.png"
-import icon2 from "../../../assets/img/homePage/nav/star.png"
-import icon3 from "../../../assets/img/homePage/nav/medicine.png"
-import icon4 from "../../../assets/img/homePage/nav/purchases.png"
-import icon5 from "../../../assets/img/homePage/nav/declaration.png"
-import icon6 from "../../../assets/img/homePage/nav/workschedule.png"
+//img
+// import icon1 from "../../../assets/img/homePage/nav/calendar.png";
+// import icon4 from "../../../assets/img/homePage/nav/purchases.png";
+// import icon5 from "../../../assets/img/homePage/nav/declaration.png";
+import icon2 from "../../../assets/img/homePage/nav/star.png";
+import icon3 from "../../../assets/img/homePage/nav/medicine.png";
+import icon6 from "../../../assets/img/homePage/nav/workschedule.png";
+import icon7 from "../../../assets/img/homePage/nav/charity.png";
 
 const links = [
-    { text: "Запис на прийом", img: icon1, path: "/"},
-    { text: "Оцінити якість наданих послуг", img: icon2, path: "/rateWork"},
-    { text: "Доступні ліки", img: icon3, path: "/"},
-    { text: "Публічні закупівлі", img: icon4, path: "/"},
-    { text: "Декларація з лікарем", img: icon5, path: "/"},
-    { text: "Графік роботи поліклініки", img: icon6, path: "/"},
+    // { text: "Запис на прийом", img: icon1, path: "/" },
+    // { text: "Публічні закупівлі", img: icon4, path: "/" },
+    // { text: "Декларація з лікарем", img: icon5, path: "/" },
+    { text: "Оцінити якість наданих послуг", img: icon2, path: "/rateWork" },
+    { text: "Доступні ліки", img: icon3, path: "/" },
+    { text: "Благодійність", img: icon7, path: "/"},
+    { text: "Графік роботи поліклініки", img: icon6, path: "/" },
 ]
 
 const HomeNav = () => {
-  return (
-    <>
-        <div className={s.homeNav}>
-            {links.map((link)=>(
-                <Link className={s.homeNav__link} href={link.path}>
-                    <Image className={s.homeNav__link_img} src={link.img} alt={link.text} width={35} height={35}/>
-                    <span className={s.homeNav__link_text}>{link.text}</span>
-                </Link>
-            ))}
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className={s.homeNav}>
+                {links.map((link) => (
+                    <Link className={s.homeNav__link} href={link.path}>
+                        <Image className={s.homeNav__link_img} src={link.img} alt={link.text} width={35} height={35} />
+                        <span className={s.homeNav__link_text}>{link.text}</span>
+                    </Link>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default HomeNav
