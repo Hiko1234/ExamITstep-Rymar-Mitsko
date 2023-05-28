0600067fb83d9dcbbc8c4ca4be83abd41b843146
Demian Rymar <ua.demyan@gmail.com>
2023-05-28 13:39:17 +0300

    WorkSchedule almost done

---
+++ src/pages/workSchedule/index.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+//import styles
+import s from './workSchedule.module.scss';
+//import components
+import Container from '@/components/container';
+
+const workSchedule = () => {
+    return (
+        <>
+            <div className={s.workSchedule}>
+                <Container>
+                    <div className={s.workSchedule__wrapper}>
+                        <div className={s.workSchedule__title}>
+                            <h1 className={s.workSchedule__title_text}>
+                                Графік роботи
+                            </h1>
+
+                            <h2 className={s.workSchedule__title_subTitle}>
+                                Графік прийому громадян адміністрацією КНП Стрийська ЦРП
+                            </h2>
+                        </div>
+
+                        <div className={s.workSchedule__info}>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <td>
+                                            Кабінет
+                                        </td>
+
+                                        <td>
+                                            Займана посада
+                                        </td>
+
+                                        <td>
+                                            П.І.П
+                                        </td>
+
+                                        <td>
+                                            Дата прийому
+                                        </td>
+
+                                        <td>
+                                            Години Прийому
+                                        </td>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            120
+                                        </td>
+
+                                        <td>
+                                            директор
+                                        </td>
+
+                                        <td>
+                                            Ігнатов О.І
+                                        </td>
+
+                                        <td>
+                                            Понеділок<br />
+                                            Четвер
+                                        </td>
+
+                                        <td>
+                                            13:00-14:00
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            401
+                                        </td>
+
+                                        <td>
+                                            нарколог
+                                        </td>
+
+                                        <td>
+                                            Лучків Б.Д
+                                        </td>
+
+                                        <td>
+                                            3-я і 4-а середа місяця
+                                        </td>
+
+                                        <td>
+                                            10:00-14:00
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            401
+                                        </td>
+
+                                        <td>
+                                            нарколог
+                                        </td>
+
+                                        <td>
+                                            Стасів Н.Б
+                                        </td>
+
+                                        <td>
+                                            2-й і 4-й четвер місяця
+                                        </td>
+
+                                        <td>
+                                            10:00-14:00
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        </>
+    )
+}
+
+export default workSchedule;
