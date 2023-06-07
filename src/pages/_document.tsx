44180bbf15a8a0b5355a96cb1c744883dbffbdd8
Demian <demyan.ua@gmail.com>
2023-06-07 15:25:54 +0300

    Site title added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Стриська центральна районна лікарня</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
