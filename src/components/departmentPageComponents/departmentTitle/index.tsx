45599ac21093b669cab76d7d7d0102c71c7d999e
Demian <demyan.ua@gmail.com>
2023-06-10 12:46:59 +0300

    Fixed department phones

--- src/components/departmentPageComponents/departmentTitle/index.tsx
+++ src/components/departmentPageComponents/departmentTitle/index.tsx
@@ -4,6 +4,8 @@ import s from "./DepartmentTitle.module.scss"
 // import img
 import Image from 'next/image';
 import phone from "../../../assets/img/departmentPage/phone.png"
+//import Link
+import Link from 'next/link';
 
 const DepartmentTitle = (props: any) => {
     const { titlePage } = props;
@@ -17,7 +19,9 @@ const DepartmentTitle = (props: any) => {
                 </div>
                 <div className={s.departmentPage__data}>
                     <h1 className={s.departmentPage__data_title}>{title}</h1>
+                    <Link className={s.departmentPage__data_link} href='tel:7-05-65'>
                     <div className={s.departmentPage__data_contacts}><Image className={s.cardDepartment__data_contactsImg} src={phone} alt="Phone number" width={16} height={16} /><span className={s.cardDepartment__data_contactsNumber}>{number}</span></div>
+                    </Link>
                 </div>
             </div>
         </>
